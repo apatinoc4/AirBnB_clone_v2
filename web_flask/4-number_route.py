fe3a08bdc5861a26466b8f731b5f7b569c9e3dc0
root <root@DESKTOP-GQSD5ER.localdomain>
2020-01-22 13:02:31 -0500

    more routes and numbered html template

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" starts a flask web app """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ display message when url is triggered """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ print HBNB when in new route """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ prints C + users input """
+    msg = 'C {}'.format(text)
+    msg = msg.replace('_', ' ')
+    return msg
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is_cool'):
+    """ inputs for python and python/text routes """
+    msg = 'Python {}'.format(text)
+    msg = msg.replace('_', ' ')
+    return msg
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """ print number only if its an int """
+    return '{} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
