8d71bcbf97b94d894f068f5e02198cf5c2d3006d
root <root@DESKTOP-GQSD5ER.localdomain>
2020-01-22 12:29:56 -0500

    more route directed inputs

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" starts a flask web app """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ display message when url is triggered """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ print HBNB when in new route """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ prints C + users input """
+    msg = 'C {}'.format(text)
+    msg = msg.replace('_', ' ')
+    return msg
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
